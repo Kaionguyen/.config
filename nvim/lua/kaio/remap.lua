79bd781238ffdd9cc47e1f6e918a0ad2d423c5ca
Kyle Nguyen <kylenguyencs@gmail.com>
2023-07-13 15:45:58 -0700

    more remaps

--- nvim/lua/kaio/remap.lua
+++ nvim/lua/kaio/remap.lua
@@ -21,8 +21,16 @@ vim.keymap.set("n", "<leader>nh", ":nohl<CR>")
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+--cursor placement
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 --buffer control
 vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
 
 --Yank to clipboard vs buffer
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])

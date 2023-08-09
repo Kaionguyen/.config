0822ed9b5d882e5cebcf7722feec20693d75e480
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-09 05:03:05 -0700

    Color matched lualine to tmux line

--- nvim/after/plugin/lualine.lua
+++ nvim/after/plugin/lualine.lua
@@ -1,8 +1,8 @@
 local custom_catppuccin = require'lualine.themes.catppuccin'
 
 -- Change the background of lualine_c section for normal mode
-custom_catppuccin.normal.c.bg = '#333333'
-
+custom_catppuccin.normal.c.bg = '#1e1e2e'
+custom_catppuccin.normal.b.bg = '#313244'
 
 require ("lualine").setup({
     options = {

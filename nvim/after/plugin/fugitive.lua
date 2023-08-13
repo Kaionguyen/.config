342b51e34662cb19acb539cfd3dd58da64aa64ec
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-13 07:41:49 -0700

    Add: new fugitive remaps

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -1,1 +1,28 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+local Fugitive_remaps = vim.api.nvim_create_augroup("Fugitive_remaps", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+    group = Fugitive_remaps,
+    pattern = "*",
+    callback = function()
+        if vim.bo.ft ~= "fugitive" then
+            return
+        end
+
+        local bufnr = vim.api.nvim_get_current_buf()
+        local opts = {buffer = bufnr, remap = false}
+        vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git('push')
+        end, opts)
+
+        -- rebase always
+        vim.keymap.set("n", "<leader>P", function()
+            vim.cmd.Git({'pull',  '--rebase'})
+        end, opts)
+    end,
+})
+
+vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gl", "<cmd>diffget //2<CR>")

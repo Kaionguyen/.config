36dba8cb7dd17eb2893520c1c3a2b7baaf74f90f
Kyle Nguyen <kylenguyencs@gmail.com>
2023-08-19 03:42:59 -0700

    Fix: oopsie woopsie

--- nvim/after/plugin/fugitive.lua
+++ nvim/after/plugin/fugitive.lua
@@ -25,4 +25,4 @@ autocmd("BufWinEnter", {
 })
 
 vim.keymap.set("n", "gh", "<cmd>diffget //2<CR>")
-vim.keymap.set("n", "gl", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gl", "<cmd>diffget //3<CR>")

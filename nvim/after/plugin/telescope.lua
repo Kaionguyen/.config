c2c6dc6769abb72e4d7ff37a6bb79f9f344cb3c2
Kyle Nguyen <kylenguyencs@gmail.com>
2023-11-07 07:31:13 -0800

    Fixed: Telescope formatting issues

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,11 +3,23 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
-vim.keymap.set('n', '<leader>cc', builtin.git_commit, {})
+vim.keymap.set('n', '<leader>cc', builtin.git_commits, {})
 
 require('telescope').setup({
     pickers = {
         find_files = {
+            theme = 'dropdown',
+        },
+        git_files = {
+            theme = 'dropdown',
+        },
+        live_grep = {
+            theme = 'dropdown',
+        },
+        help_tags = {
+            theme = 'dropdown',
+        },
+        git_commits = {
             theme = 'dropdown',
         },
     },

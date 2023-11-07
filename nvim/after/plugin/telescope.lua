623c2c9b7094b30a7d937c86cd7ea45c66aaddcb
Kyle Nguyen <kylenguyencs@gmail.com>
2023-11-07 07:13:18 -0800

    Add: Commit picker

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -3,3 +3,12 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>gr', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>cc', builtin.git_commit, {})
+
+require('telescope').setup({
+    pickers = {
+        find_files = {
+            theme = 'dropdown',
+        },
+    },
+})

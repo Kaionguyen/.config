da9687c4ef6b6f8fac4acd4820b82970459a8f41
Kyle Nguyen <kylenguyencs@gmail.com>
2025-01-18 14:15:35 -0800

    feat: new mark bind

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -28,7 +28,7 @@ harpoon:setup({
 -- vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
 -- { desc = "Open harpoon window" })
 
-vim.keymap.set("n", "<C-m>", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
